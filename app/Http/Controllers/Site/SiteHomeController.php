44bcf565e8e6975c3e2c6daa947558be13dd4490
namnguyen3 <namnguyen3@beeketing.net>
2023-10-06 07:02:18 +0700

    fix bug

--- app/Http/Controllers/Site/SiteHomeController.php
+++ app/Http/Controllers/Site/SiteHomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
-use App\Services\CustomerService;
+use App\Services\UserService;
 use App\Services\OrderActivityService;
 use App\Services\OrderListService;
 use App\Services\SettingService;
@@ -14,14 +14,14 @@ use App\Helpers\Location;
 class SiteHomeController extends Controller
 {
   public function __construct(
-    CustomerService $customer,
+    UserService $user,
     OrderActivityService $orderActivity,
     OrderListService $orderList,
     SettingService $setting,
     RequestService $requestService
   )
   {
-    $this->customer = $customer;
+    $this->user = $user;
     $this->activity = $orderActivity;
     $this->order = $orderList;
     $this->setting = $setting;
@@ -43,44 +43,44 @@ class SiteHomeController extends Controller
 
   public function dashboard()
   {
-    $customer = $this->customer->info();
+    $user = $this->user->info();
     
-    $activity = $this->getActivityByCustomer($customer);
+    $activity = $this->getActivityByCustomer($user);
     
-    $newCustomer = $this->getNewData($customer);
+    $newCustomer = $this->getNewData($user);
 
     return view('site.home.dashboard')
-      ->with('customer', $customer)
+      ->with('customer', $user)
       ->with('activity', $activity)
       ->with('newCustomer', $newCustomer);
   }
 
-  public function getActivityByCustomer($customer)
+  public function getActivityByCustomer($user)
   {
     $activity = [];
     $limit = 10;
     $company = [];
-    if ($this->customer->isMkt()) {
-      $company = $customer->load('companyMkt')->companyMkt->pluck('company_sale_id')->toArray();
+    if ($this->user->isMkt()) {
+      $company = $user->load('companyMkt')->companyMkt->pluck('company_sale_id')->toArray();
       
     }
-    array_push($company, $customer->company_id);
+    array_push($company, $user->company_id);
     // dd($company);
-    $userId = $this->customer->getByCompanyId($company)->pluck('_id')->toArray();
-    if ($this->customer->isAdmin()) {
+    $userId = $this->user->getByCompanyId($company)->pluck('_id')->toArray();
+    if ($this->user->isAdmin()) {
       $activity = $this->activity->getByUserId($userId)->where('group', '!=', 2)->take($limit);
     } else {
-      $activity = $customer->load('activity')->activity->where('group', '!=', 2)->take($limit);
+      $activity = $user->load('activity')->activity->where('group', '!=', 2)->take($limit);
     }
     return $activity;
   }
 
-  public function getNewData($customer)
+  public function getNewData($user)
   {
-    if ($this->customer->isMkt()){
-      $companys =  $customer->company_id;
+    if ($this->user->isMkt()){
+      $companys =  $user->company_id;
     }else{
-      $companys = $this->setting->getCompanyConnectFromSale($customer->company_id)->pluck('company_mkt_id')->toArray();
+      $companys = $this->setting->getCompanyConnectFromSale($user->company_id)->pluck('company_mkt_id')->toArray();
     }
     return $this->order->getByCompanyId($companys, 10 /* limit */);
   }
